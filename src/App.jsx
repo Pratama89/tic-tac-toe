42810c71a7b0791519b35aa905a92bcd017e1c71
Pratama89 <pratama.adiciptawan@gmail.com>
2023-07-18 08:40:10 +0700

    tic tac

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+function Kotak() {
+  return <button className='kotak'>X</button>
+}
+
+function App() {
+  
+  return (
+    <>
+      <Kotak />
+    </>
+  )
+}
+
+export default App
